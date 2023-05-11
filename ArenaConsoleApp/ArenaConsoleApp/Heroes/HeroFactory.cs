a6caeeb9845f0b916f7dedfa109424ae6a5cffee
Rajkai Bende <bendr424@gmail.com>
2023-05-11 20:25:26 +0200

    Added hero factory

---
+++ ArenaConsoleApp/ArenaConsoleApp/Heroes/HeroFactory.cs
@@ -0,0 +1,44 @@
+﻿namespace ArenaConsoleApp.Heroes
+{
+    internal class HeroFactory
+    {
+        private int _archerId = 1;
+        private int _knightId = 1;
+        private int _swordsmanId = 1;
+
+        private delegate IHero HeroFactoryFunction();
+        private HeroFactoryFunction[] _heroFactories;
+
+        public HeroFactory()
+        {
+            _heroFactories = new HeroFactoryFunction[]
+            {
+                new HeroFactoryFunction(CreateArcher),
+                new HeroFactoryFunction(CreateKnight),
+                new HeroFactoryFunction(CreateSwordsman),
+            };
+        }
+
+        public IHero CreateArcher()
+        {
+            return new Archer($"Archer {_archerId++}");
+        }
+
+        public IHero CreateKnight()
+        {
+            return new Knight($"Knight {_knightId++}");
+        }
+
+        public IHero CreateSwordsman()
+        {
+            return new Swordsman($"Swordsman {_swordsmanId++}");
+        }
+
+        public IHero CreateRandomHero()
+        {
+            var random = new Random();
+            var index = random.Next(_heroFactories.Length);
+            return _heroFactories[index]();
+        }
+    }
+}
--- ArenaConsoleApp/ArenaConsoleApp/Program.cs
+++ ArenaConsoleApp/ArenaConsoleApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArenaConsoleApp.Heroes;
+
+// Composition root
+var heroFactory = new HeroFactory();
+
+
+// Start of application
+int numberOfHeroes = 0;
+bool isUserInputInt = false;
+while(!isUserInputInt){
+    Console.Write("How many heroes do you want? ");
+    isUserInputInt = int.TryParse(Console.ReadLine(), out numberOfHeroes);
+}
+
+var heroes = CreateHeroes(numberOfHeroes, heroFactory);
+
+
+Console.ReadLine();
+
+
+IEnumerable<IHero> CreateHeroes(int numberOfHeroes, HeroFactory heroFactory)
+{
+    var heroes = new List<IHero>();
+
+    for (int i = 0; i < numberOfHeroes; i++)
+    {
+        heroes.Add(heroFactory.CreateRandomHero());
+    }
+
+    return heroes;
+}
